088821e2db11c9e4f46252acb761a439dd508bf3
LXiao2015 <m15801021028@163.com>
2018-05-07 09:51:33 +0800

    Fixed bugs

--- www/orm.py
+++ www/orm.py
@@ -247,7 +247,7 @@ class Model(dict, metaclass=ModelMetaclass):
 
 	async def remove(self):
 		args = [self.getValue(self.__primary_key__)]
-		rows = await execute(self.__remove__, args)
+		rows = await execute(self.__delete__, args)
 		if rows != 1:
 			logging.warn('failed to remove by primary key: affected rows: %s' % rows)
 
